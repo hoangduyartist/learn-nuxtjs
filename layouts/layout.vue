14fbc9e2f81fce41ac4dae818f78726e2006e3eb
hoangduyartist <hoangduy.artist@gmail.com>
2020-09-23 17:39:02 +0700

    commit first nuxtjs project

---
+++ layouts/layout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <!-- <TheHeader /> -->
+    <h2>Layout header</h2>
+    <nuxt />
+    <h2>Layout footer</h2>
+    <!-- <TheFooter /> -->
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
